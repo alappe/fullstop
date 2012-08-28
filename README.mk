ed133dad3bdd658eb9ed2f971d6e5269808d0758
Andreas Lappe <nd@off-pist.de>
2012-08-28 14:31:07 +0200

    [TASK] Add a README.

---
+++ README.mk
@@ -0,0 +1,17 @@
+fullstop
+========
+
+## Synopsis
+
+Being tired of adding my dotfiles by hand on every box I get a new
+account on, fullstop pulls down it's repostitory and with it all
+submodules. It then links all files mentioned in the .fullstoprc
+configuration files into the homedirectory.
+
+## Usage
+
+`curl https://raw.github.com/alappe/fullstop/master/bin/fullstop | ruby`
+
+## License
+
+BSD… it's a real simple script…
